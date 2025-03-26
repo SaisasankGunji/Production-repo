3d869df35c44fa3092fe8bb6a2b10d876ea8aa57
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-03-26 09:31:52 +0530

    Update userService.js
    Updated the .env import

--- frontend/src/services/users/userService.js
+++ frontend/src/services/users/userService.js
@@ -1,72 +1,55 @@
 import axios from "axios";
-// import { BASE_URL } from "../../utils/url";
 import { getUserFromStorage } from "../../utils/getUserFromStorage";
 
-// Get the token
-const token = getUserFromStorage();
+// Get Base URL from .env
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // Fetch user profile
 export const getUserProfileAPI = async () => {
-  const response = await axios.get(
-    `${process.env.VITE_BASE_URL}/users/profile`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  const token = getUserFromStorage();
+  const response = await axios.get(`${API_BASE_URL}/users/profile`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data;
 };
 
 // Login
 export const loginAPI = async ({ email, password }) => {
-  const response = await axios.post(
-    `${process.env.VITE_BASE_URL}/users/login`,
-    {
-      email,
-      password,
-    }
-  );
+  const response = await axios.post(`${API_BASE_URL}/users/login`, {
+    email,
+    password,
+  });
   return response.data;
 };
 
 // Register
 export const registerAPI = async ({ email, password, username }) => {
-  const response = await axios.post(
-    `${process.env.VITE_BASE_URL}/users/register`,
-    {
-      email,
-      password,
-      username,
-    }
-  );
+  const response = await axios.post(`${API_BASE_URL}/users/register`, {
+    email,
+    password,
+    username,
+  });
   return response.data;
 };
 
 // Change password
 export const changePasswordAPI = async (newPassword) => {
+  const token = getUserFromStorage();
   const response = await axios.put(
-    `${process.env.VITE_BASE_URL}/users/change-password`,
+    `${API_BASE_URL}/users/change-password`,
     { newPassword },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
+    { headers: { Authorization: `Bearer ${token}` } }
   );
   return response.data;
 };
 
 // Update profile
 export const updateProfileAPI = async ({ email, username }) => {
+  const token = getUserFromStorage();
   const response = await axios.put(
-    `${process.env.VITE_BASE_URL}/users/update-profile`,
+    `${API_BASE_URL}/users/update-profile`,
     { email, username },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
+    { headers: { Authorization: `Bearer ${token}` } }
   );
   return response.data;
 };
